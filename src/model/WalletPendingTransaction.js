e99a6dcc7377282df9deb3715831c7de810e4708
nkrivous <kolsln@list.ru>
2018-11-26 13:39:17 +0000

    version update

--- src/model/WalletPendingTransaction.js
+++ src/model/WalletPendingTransaction.js
@@ -130,16 +130,16 @@ export default class WalletPendingTransaction {
     static CurrencyEnum = {
     
         /**
-         * value: "GVT"
-         * @const
-         */
-        "GVT": "GVT",
-    
-        /**
          * value: "Undefined"
          * @const
          */
         "Undefined": "Undefined",
+    
+        /**
+         * value: "GVT"
+         * @const
+         */
+        "GVT": "GVT",
     
         /**
          * value: "ETH"
