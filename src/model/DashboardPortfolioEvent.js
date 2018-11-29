51daba860550d466fe4250477ab57dd59de8a3d5
nkrivous <kolsln@list.ru>
2018-11-29 17:15:29 +0000

    version update

--- src/model/DashboardPortfolioEvent.js
+++ src/model/DashboardPortfolioEvent.js
@@ -144,16 +144,16 @@ export default class DashboardPortfolioEvent {
     static CurrencyEnum = {
     
         /**
-         * value: "GVT"
-         * @const
-         */
-        "GVT": "GVT",
-    
-        /**
          * value: "Undefined"
          * @const
          */
         "Undefined": "Undefined",
+    
+        /**
+         * value: "GVT"
+         * @const
+         */
+        "GVT": "GVT",
     
         /**
          * value: "ETH"
