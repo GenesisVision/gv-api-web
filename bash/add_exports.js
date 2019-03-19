ed82eb1225db088b05a5379a4832aac888257c93
Евгений Гребенщиков <evgreb@gmail.com>
2019-03-19 17:13:09 +0300

    fix types

--- bash/add_exports.js
+++ bash/add_exports.js
@@ -2,3 +2,5 @@ const shell = require("shelljs");
 
 shell.sed("-i", /declare/, "export declare", "distribution/types.d.ts");
 shell.sed("-i", /changeabort: any;/, "abort: (data: T) => CancelablePromise<T>", "distribution/types.d.ts");
+shell.sed("-i", /'String'/, "string", "distribution/types.d.ts");
+shell.sed("-i", /'Number'/, "number", "distribution/types.d.ts");
