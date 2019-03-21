2c55da2b063f12ea74ddc8684edad1f3cc3fae5f
Евгений Гребенщиков <evgreb@gmail.com>
2019-03-21 17:05:58 +0300

    change cancel method

--- bash/add_exports.js
+++ bash/add_exports.js
@@ -1,6 +1,6 @@
 const shell = require("shelljs");
 
 shell.sed("-i", /declare/, "export declare", "distribution/types.d.ts");
-shell.sed("-i", /changecancel: any;/, "cancel: (data: T) => CancelablePromise<T>", "distribution/types.d.ts");
+shell.sed("-i", /changecancel: any;/, "cancel: () => CancelablePromise<T>", "distribution/types.d.ts");
 shell.sed("-i", /'String'/, "string", "distribution/types.d.ts");
 shell.sed("-i", /'Number'/, "number", "distribution/types.d.ts");
