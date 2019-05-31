6b48a06fea6e9c7e465120860019ae7ed5383683
nkrivous <kolsln@list.ru>
2019-05-31 11:20:09 +0000

    version update

--- distribution/model/ProgramDetails.js
+++ distribution/model/ProgramDetails.js
@@ -297,7 +297,7 @@ var ProgramDetails = function () {
 
 /**
  * @typedef ProgramDetailsCurrencyEnum 
- * @type {("Undefined"|"GVT"|"ETH"|"BTC"|"ADA"|"USDT"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
+ * @type {("BTC"|"ETH"|"USDT"|"GVT"|"Undefined"|"ADA"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
  */
 
 /**
@@ -308,17 +308,17 @@ var ProgramDetails = function () {
 
 ProgramDetails.CurrencyEnum = {
 
-    "Undefined": "Undefined",
-
-    "GVT": "GVT",
+    "BTC": "BTC",
 
     "ETH": "ETH",
 
-    "BTC": "BTC",
+    "USDT": "USDT",
+
+    "GVT": "GVT",
+
+    "Undefined": "Undefined",
 
     "ADA": "ADA",
-
-    "USDT": "USDT",
 
     "XRP": "XRP",
 
