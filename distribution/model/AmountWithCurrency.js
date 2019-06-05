31863f2afab21f3d36882d072a522f60c9247ed0
nkrivous <kolsln@list.ru>
2019-06-05 10:17:50 +0000

    version update

--- distribution/model/AmountWithCurrency.js
+++ distribution/model/AmountWithCurrency.js
@@ -71,23 +71,23 @@ var AmountWithCurrency = function () {
 
 /**
  * @typedef AmountWithCurrencyCurrencyEnum 
- * @type {("BTC"|"ETH"|"USDT"|"GVT"|"Undefined"|"ADA"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
+ * @type {("Undefined"|"GVT"|"ETH"|"BTC"|"ADA"|"USDT"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
  */
 
 
 AmountWithCurrency.CurrencyEnum = {
 
-    "BTC": "BTC",
-
-    "ETH": "ETH",
-
-    "USDT": "USDT",
+    "Undefined": "Undefined",
 
     "GVT": "GVT",
 
-    "Undefined": "Undefined",
+    "ETH": "ETH",
+
+    "BTC": "BTC",
 
     "ADA": "ADA",
+
+    "USDT": "USDT",
 
     "XRP": "XRP",
 
