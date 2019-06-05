9bcededb57469aad3f858cbc94f6f3717b4f40a7
nkrivous <kolsln@list.ru>
2019-06-05 16:54:33 +0000

    version update

--- src/model/ProgramBalanceChart.js
+++ src/model/ProgramBalanceChart.js
@@ -98,17 +98,17 @@ export default class ProgramBalanceChart {
 
     static ProgramCurrencyEnum = {
     
-        "BTC": "BTC",
-    
-        "ETH": "ETH",
-    
-        "USDT": "USDT",
+        "Undefined": "Undefined",
     
         "GVT": "GVT",
     
-        "Undefined": "Undefined",
+        "ETH": "ETH",
+    
+        "BTC": "BTC",
     
         "ADA": "ADA",
+    
+        "USDT": "USDT",
     
         "XRP": "XRP",
     
@@ -134,7 +134,7 @@ export default class ProgramBalanceChart {
 
 /**
  * @typedef ProgramBalanceChartProgramCurrencyEnum 
- * @type {("BTC"|"ETH"|"USDT"|"GVT"|"Undefined"|"ADA"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
+ * @type {("Undefined"|"GVT"|"ETH"|"BTC"|"ADA"|"USDT"|"XRP"|"BCH"|"LTC"|"DOGE"|"BNB"|"USD"|"EUR")}
  */
 
 
