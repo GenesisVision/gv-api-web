5b44d894ded546e567a0964ebb74f73d09ae0cad
nkrivous <kolsln@list.ru>
2019-06-23 08:26:25 +0000

    version update

--- src/types.d.ts
+++ src/types.d.ts
@@ -2381,7 +2381,7 @@ export declare interface SignalsList {
     total: number;
 }
 
-export declare type SocialLinkViewModelTypeEnum = "Undefined" | "Twitter" | "Telegram" | "Facebook" | "LinkedIn" | "Youtube" | "WeChat" | "Email";
+export declare type SocialLinkViewModelTypeEnum = "Undefined" | "Email" | "WeChat" | "Youtube" | "Facebook" | "Telegram" | "Twitter" | "LinkedIn";
 
 export declare interface SocialLinkViewModel {
     url: string;
@@ -2473,7 +2473,7 @@ export declare interface UpdateProfileViewModel {
     about: string;
 }
 
-export declare type UpdateSocialLinkViewModelTypeEnum = "Undefined" | "Twitter" | "Telegram" | "Facebook" | "LinkedIn" | "Youtube" | "WeChat" | "Email";
+export declare type UpdateSocialLinkViewModelTypeEnum = "Undefined" | "Email" | "WeChat" | "Youtube" | "Facebook" | "Telegram" | "Twitter" | "LinkedIn";
 
 export declare interface UpdateSocialLinkViewModel {
     type: UpdateSocialLinkViewModelTypeEnum;
