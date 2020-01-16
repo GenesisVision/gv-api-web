90cce1eeb2f70eabe9bff58d33532462e21c4435
e.grebenshikov <evgreb@gmail.com>
2020-01-16 14:22:48 +0300

    injected fetch

--- src/ApiClient.ts
+++ src/ApiClient.ts
@@ -1,10 +1,21 @@
 export default class ApiClient {
-    apiUrl: string;
-    constructor(apiUrl: string = "https://localhost/api") {
-        this.apiUrl = apiUrl;
-    }
+  readonly apiUrl: string;
+  private readonly fetchMethod?: (
+    input: RequestInfo,
+    init?: RequestInit
+  ) => Promise<Response>;
+  constructor(
+    apiUrl: string = "https://localhost/api",
+    fetchMethod?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+  ) {
+    this.fetchMethod = fetchMethod;
+    this.apiUrl = apiUrl;
+  }
 
-    fetch(input: RequestInfo, init?: RequestInit) {
-        return fetch(input, init);
+  fetch(input: RequestInfo, init?: RequestInit) {
+    if (this.fetchMethod !== undefined) {
+      return this.fetchMethod(input, init);
     }
+    return fetch(input, init);
+  }
 }
