3b88df326bb03aef6d92f347b0ecf110b134f4fc
e.grebenshikov <evgreb@gmail.com>
2020-01-17 15:43:52 +0300

    update api

--- src/model/BrokerTradeServerType.ts
+++ src/model/BrokerTradeServerType.ts
@@ -1,1 +1,1 @@
-export type BrokerTradeServerType = 'Undefined' | 'MetaTrader4' | 'MetaTrader5' | 'NinjaTrader' | 'cTrader' | 'Rumus' | 'Metastock' | 'IDEX' | 'Huobi' | 'Exante' | 'BinanceFollow';
+export type BrokerTradeServerType = 'MetaTrader5' | 'Undefined' | 'MetaTrader4' | 'NinjaTrader' | 'cTrader' | 'Rumus' | 'Metastock' | 'IDEX' | 'Huobi' | 'Exante' | 'BinanceFollow';
